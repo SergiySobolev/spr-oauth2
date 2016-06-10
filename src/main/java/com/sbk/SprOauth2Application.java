c2f735e374b5ef0a40b5dd48c15965098414ce1e
SergiySobolev <sergiy.sobolev@gmail.com>
2016-06-10 21:01:32 +0300

    logged in with facebook

--- src/main/java/com/sbk/SprOauth2Application.java
+++ src/main/java/com/sbk/SprOauth2Application.java
@@ -3,12 +3,23 @@ package com.sbk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
+@RestController
 public class SprOauth2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprOauth2Application.class, args);
 	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
 }
